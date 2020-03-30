6016b08c1ebb60f3afc24a89b3d78ffef7b4a1a4
Eric Yu <ericfish@gmail.com>
2020-03-30 17:47:40 +0800

    init

---
+++ 01-remarks-ui/index.js
@@ -0,0 +1,88 @@
+const { ApiPromise, WsProvider } = require('@polkadot/api');
+import {
+	isWeb3Injected,
+	web3Accounts,
+	web3Enable,
+	web3FromAddress
+} from "@polkadot/extension-dapp";
+web3Enable('polkadot-js/apps');
+
+// Samples
+class PolkadotWeb3JSSample {
+	/***
+	 * login
+	 * @return accounts [{"address":"5D2JMakX2CgtPPkiqUzdsK3Y41vD6HyNy8ZETUjhjRrZFTfG","meta":{"name":"cc1","source":"polkadot-js"}}]
+
+	 */
+	async login() {
+		if (!isWeb3Injected) {
+			throw new Error("Please install/unlock the MathWallet first");
+		}
+		// meta.source contains the name of the extension that provides this account
+		const allAccounts = await web3Accounts();
+		return allAccounts;
+	}
+
+	/***
+	 * Transfer
+	 * @param from from
+	 * @param to to
+	 * @param amount amount
+	 * @return hash
+	 */
+	async transfer(from, to, amount) {
+
+		//////////////////////////// alexander  ////////////////////////////
+		// Initialise the provider to connect to the local node
+		const provider = new WsProvider('ws://localhost:9944');
+		// Create the API and wait until ready
+		const api = await ApiPromise.create({ provider });
+
+		// finds an injector for an address
+		const injector = await web3FromAddress(from);
+
+		// sets the signer for the address on the @polkadot/api
+		api.setSigner(injector.signer);
+
+		// sign and send out transaction - notice here that the address of the account (as retrieved injected)
+		// is passed through as the param to the `signAndSend`, the API then calls the extension to present
+		// to the user and get it signed. Once completex, the api sends the tx + signature via the normal process
+		const h = api.tx.balances
+			.transfer(to, amount)
+			.signAndSend(from);
+
+		return h;
+	}
+
+	/***
+	 * Remark
+	 * @param from from
+	 * @param content content
+	 * @return hash
+	 */
+	async remark(from, content) {
+
+		//////////////////////////// alexander  ////////////////////////////
+		// Initialise the provider to connect to the local node
+		const provider = new WsProvider('ws://localhost:9944');
+		// Create the API and wait until ready
+		const api = await ApiPromise.create({ provider });
+
+		// finds an injector for an address
+		const injector = await web3FromAddress(from);
+
+		// sets the signer for the address on the @polkadot/api
+		api.setSigner(injector.signer);
+
+		// sign and send out transaction - notice here that the address of the account (as retrieved injected)
+		// is passed through as the param to the `signAndSend`, the API then calls the extension to present
+		// to the user and get it signed. Once completex, the api sends the tx + signature via the normal process
+		const h = api.tx.system
+			.remark(content)
+			.signAndSend(from);
+
+		return h;
+	}
+}
+
+window.PolkadotWeb3JSSample = new PolkadotWeb3JSSample();
